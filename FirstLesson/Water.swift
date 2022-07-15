64ab45bc19ef87254769166c60282b2b635c97f0
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2022-07-15 19:04:38 +0300

    dismiss barcode

--- FirstLesson/Water.swift
+++ FirstLesson/Water.swift
@@ -12,5 +12,9 @@ class Water: Product {
     override func infoAboutProduct() {
         print("\(name) по цене \(price)")
     }
+    
+    init(name: String, price: Int) {
+        super.init(name: name, price: price , barcode: 3415616)
+    }
 }
 
--- FirstLesson/main.swift
+++ FirstLesson/main.swift
@@ -10,12 +10,13 @@ import Foundation
 let magnit = Shop(name: "Magnit")
 
 let chocolate = Chocolate(name: "Kinder", price: 100, barcode: 123456)
-let water = Water(name: "Sprite", price: 135, barcode: 3415616)
+let water = Water(name: "Sprite", price: 135)
 let chips = Chips(name: "Lays", price: 70, barcode: 220491)
 
 magnit.addProductToBasket(product: water, howMany: 2)
+magnit.removeFromBasket(barcode: water.barcode)
+magnit.addProductToBasket(product: chips, howMany: 4)
 
-//magnit.removeFromBasket(barcode: 3415616)
 chips.infoAboutProduct()
 water.infoAboutProduct()
 
